4637f614c148a873e6c9088c76a02c0ae39f78e7
Ionut Chirita <ionut_chirita@yahoo.com>
2023-08-03 23:52:09 +0300

    fetching data with javascript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testing');
+let quotes = [];
+
+function newQuote() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    console.log(quote);
+}
+
+// get the quotes
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        quotes = await response.json();
+        // console.log(quotes[12]);
+        newQuote();
+    } catch (error) {
+        alert(error);
+    }
+
+}
+
+getQuotes();
