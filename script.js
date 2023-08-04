065329e2fb9facb94e174d86a15eda4231c160e1
Ionut Chirita <ionut_chirita@yahoo.com>
2023-08-04 11:10:00 +0300

    manipulatin DOM with JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let quotes = [];
 
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const quoteTwitter = document.getElementById('twitter');
+const quoteNewBtn = document.getElementById('new-quote');
+
+// get the quote
 function newQuote() {
     const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    console.log(quote);
+
+    // check if quote is long
+    if (quoteText.textContent.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+
+    // check if author exist
+    if (quote.author) {
+        quoteAuthor.textContent = quote.author;
+    } else {
+        quoteAuthor.textContent = 'Undefined';
+    }
 }
 
 // get the quotes
@@ -18,4 +39,14 @@ async function getQuotes() {
 
 }
 
+function tweetQuote() {
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(tweetUrl, '_blank');
+}
+
+//add event listeners
+quoteTwitter.addEventListener('click', tweetQuote);
+quoteNewBtn.addEventListener('click', newQuote);
+
+// On Load
 getQuotes();
